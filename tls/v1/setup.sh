2e9ccae5d5976868b491a5b71b13b4ac73bb1b2f
amin-jafarpour <a.jafarpour2001@gmail.com>
2025-07-31 14:57:52 -0700

    removed tls directory from dev branch

--- tls/v1/setup.sh
+++
@@ -1,9 +0,0 @@
-sudo apt install openssl build-essential pkg-config libssl-dev 
-
-openssl req -x509 -newkey rsa:4096 -sha256 -nodes -days 365 -keyout server.key -out server.crt -subj "/CN=localhost" 
-
-gcc -Wall -Wextra -o server_tls server_tls.c -lssl -lcrypto 
-
-gcc -Wall -Wextra -o client_tls client_tls.c -lssl -lcrypto 
-
-
